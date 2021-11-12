cdff6053d90bdfaf2ba1751a673b56b7230fe7fe
Alan Schelten <alan.schelten@mercateo.com>
2021-11-12 17:17:26 +0100

    Document

--- src/rowwise.jl
+++ src/rowwise.jl
@@ -2,16 +2,56 @@ mutable struct recordwriter
     io :: IO
     schtyp :: SchemaType
     sync :: NTuple{16, UInt8}
-
-    function recordwriter(schema::Tables.Schema, pth:: String)
-        io = open(pth,"w")
-        schtyp = schematype(schema)
-        writeheader(io, schtyp)
-        sync = _cast(NTuple{16, UInt8}, rand(UInt128))
-        return new(io, schtyp, sync)
-    end
 end
 
+
+"""
+recordwriter(schema::Tables.Schema, pth:: String)
+
+Opens file handle at `pth` and writes `schema` as header. 
+To write records as separate partitions use `writerecord(arr::recordwriter, record)` 
+and call close(arr::recordwriter) to close file handle.
+
+```julia
+mktempdir() do p
+pth = joinpath(p,"tmp.avro")
+sch = Tables.Schema([:a, :b, :c, :d], 
+    [Union{Missing, Int64}, Float64, Union{Missing, Int64}, Union{Int64, String}])
+rw = Avro.recordwriter(sch, pth) 
+Avro.writerecord(rw, (a=missing, b=2., c=3, d=4))
+Avro.writerecord(rw, (a=1, b=4.0, c=missing, d=5))
+Avro.writerecord(rw, (a=3, b=3.7, c=3, d="5"))
+close(rw)
+end
+```
+"""
+function recordwriter(schema::Tables.Schema, pth:: String)
+    io = open(pth,"w")
+    schtyp = schematype(schema)
+    writeheader(io, schtyp)
+    sync = _cast(NTuple{16, UInt8}, rand(UInt128))
+    return recordwriter(io, schtyp, sync)
+end
+
+"""
+recordwriter(f::Function, schema::Tables.Schema, pth:: String)
+
+Writes `schema` as header to `pth` and ensures file handle is closed.
+To write records as separate partitions use `writerecord(arr::recordwriter, record)`.
+
+```julia
+mktempdir() do p
+    pth = joinpath(p,"tmp.avro")
+    sch = Tables.Schema([:a, :b, :c, :d], 
+        [Union{Missing, Int64}, Float64, Union{Missing, Int64}, Union{Int64, String}])
+    Avro.recordwriter(sch, pth) do rw
+        Avro.writerecord(rw, (a=missing, b=2., c=3, d=4))
+        Avro.writerecord(rw, (a=1, b=4.0, c=missing, d=5))
+        Avro.writerecord(rw, (a=3, b=3.7, c=3, d="5"))
+    end
+end
+```
+"""
 function recordwriter(f::Function, schema::Tables.Schema, pth:: String)
     writer = recordwriter(schema, pth)
     try
@@ -49,18 +89,66 @@ mutable struct recordreader
     len :: Int
     io :: IO
     comp
+end
 
-    function recordreader(pth:: String)
-        fh = open(pth)
-        buf = Mmap.mmap(fh)
-        header, pos = readvalue(Binary(), FileHeaderRecordType, FileHeader, buf, 1, length(buf), nothing)
-        sch = JSON3.read(header.meta["avro.schema"], Schema)
-        comp = get(header.meta, "avro.codec", nothing)
-        comp = get(DECOMPRESSORS, Symbol(comp), nothing)
-        T = juliatype(sch)
-        len = length(buf)
-        schtype = schematype(T)
-        return new(buf, pos, pos, T, schtype, len, fh, comp)
+"""
+recordreader(pth:: String)
+
+Open avro file and read header. Use `iterate` to access one partition at a time.
+Close filehandle with `close(arr::recordreader)`
+
+```julia
+mktempdir() do p
+    pth = joinpath(p,"tmp.avro")
+    sch = Tables.Schema([:a, :b], [Int, Int])
+    Avro.recordwriter(sch, pth) do rw
+        Avro.writerecord(rw, (a=1, b=2))
+        Avro.writerecord(rw, (a=1, b=4))
+    end
+    rr = Avro.recordreader(pth)
+    @assert NamedTuple(first(rr)) == (a=1, b=2)
+    close(rr)
+end
+```
+"""
+function recordreader(pth:: String)
+    fh = open(pth)
+    buf = Mmap.mmap(fh)
+    header, pos = readvalue(Binary(), FileHeaderRecordType, FileHeader, buf, 1, length(buf), nothing)
+    sch = JSON3.read(header.meta["avro.schema"], Schema)
+    comp = get(header.meta, "avro.codec", nothing)
+    comp = get(DECOMPRESSORS, Symbol(comp), nothing)
+    T = juliatype(sch)
+    len = length(buf)
+    schtype = schematype(T)
+    return recordreader(buf, pos, pos, T, schtype, len, fh, comp)
+end
+
+"""
+    recordreader(f::Function, pth:: String)
+
+Open avro file and read header. Use `iterate` to access one partition at a time.
+
+```julia
+mktempdir() do p
+    pth = joinpath(p,"tmp.avro")
+    sch = Tables.Schema([:a, :b], [Int, Int])
+    Avro.recordwriter(sch, pth) do rw
+        Avro.writerecord(rw, (a=1, b=2))
+        Avro.writerecord(rw, (a=1, b=4))
+    end
+    Avro.recordreader(pth) do rr
+        @assert NamedTuple(first(rr)) == (a=1, b=2)
+    end
+end
+```
+"""
+function recordreader(f::Function, pth:: String)
+    reader = recordreader(pth)
+    try
+        f(reader)
+    finally
+        close(reader)
     end
 end
 
@@ -91,15 +179,6 @@ function read(reader::recordreader)
     end
     v, _ = readvalue(Binary(), reader.schtype, reader.T, bytes, 1, length(bytes), nothing)
     return v
-end
-
-function recordreader(f::Function, pth:: String)
-    reader = recordreader(pth)
-    try
-        f(reader)
-    finally
-        close(reader)
-    end
 end
 
 function close(arr::recordreader)
